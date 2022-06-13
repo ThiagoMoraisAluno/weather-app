385a8fb4ca16c8b79c9f5f892b4c56113f7bd4d5
ThiagoMoraisAluno <thiagomoraiscordeiro@outlook.com>
2022-06-13 18:40:40 -0300

    usando o useState para a criação do estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import fetchData from './services/api';
 import Card from './components/Card';
 function App() {
+  const [city, setCity] = useState('');
+  const [data, setData] = useState({});
 
     const handleSubmit = (event) =>{
       event.preventDefault();
-      console.log (fetchData('fortaleza'));
+       fetchData(city).then((response) => {
+        setData(response);
+
+       });
       
-    }
+    };
 
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center">
@@ -16,14 +21,16 @@ function App() {
         type="text" 
         placeholder='cidade' 
         className='p-3 rounded-lg outline-none'
+        value={city}
+        onChange={({ target: { value } }) => setCity(value)} 
         />
         <button 
         type="submit"
         className='bg-blue-600 p-3  rounded-lg ml-3 text-white font-bold'
         >Pesquisar</button>
       </form>
-
-      <Card />
+        
+      <Card data={data} />
 
     </div>
   )
--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,13 @@ const KEY = 'be2eac40a37f42e6b81192404221306';
 
 const fetchData = async (city) => {
 
-    const url = 'http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no'
+    const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`
 
     const fecthResponse = await fetch(url);
 
-    return fecthResponse;
+    const data = await fecthResponse.json();
+
+    return data;
 }
 
 export default fetchData;
