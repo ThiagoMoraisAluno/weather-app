e9c8a5d8d3cc062dde9c0a3605aa630b152fed8a
ThiagoMoraisAluno <thiagomoraiscordeiro@outlook.com>
2022-06-13 16:20:15 -0300

    estilização do formulário de pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import Card from './components/Card';
 function App() {
   return (
-    <div className="flex w-full h-screen items-center justify-center">
+    <div className="flex flex-col w-full h-screen items-center justify-center">
       <form>
-        <input type="text" placeholder='cidade' />
-        <button type="submit">Pesquisar</button>
+        <input 
+        type="text" 
+        placeholder='cidade' 
+        className='p-3 rounded-lg outline-none'
+        />
+        <button 
+        type="submit"
+        className='bg-blue-600 p-3  rounded-lg ml-3 text-white font-bold'
+        >Pesquisar</button>
       </form>
+
+      <Card />
+
     </div>
   )
 }
